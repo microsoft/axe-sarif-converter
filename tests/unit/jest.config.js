e0ecdad40241c3f6c5d5c90c8c9f456c0b8e22ca
Shobhit Agarwal <shobhit657@gmail.com>
2019-04-01 11:49:51 -0700

    Add jest config and setup

---
+++ tests/unit/jest.config.js
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const rootDir = '../../';
+const currentDir = '<rootDir>/tests/unit';
+
+module.exports = {
+    clearMocks: true,
+    testRunner: 'jest-circus/runner',
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    verbose: false,
+    coverageDirectory: '<rootDir>/test-results/unit/coverage',
+    displayName: 'unit tests',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    rootDir: rootDir,
+    roots: [currentDir],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}',
+        '!<rootDir>/src/tests/**/*',
+        '!<rootDir>/src/**/*.d.ts',
+    ],
+    coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
+    testEnvironment: 'jsdom',
+    testMatch: [`${currentDir}/**/*.test.(ts|tsx|js)`],
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                outputDirectory: '.',
+                outputName: '<rootDir>/test-results/unit/junit.xml',
+            },
+        ],
+    ],
+};
