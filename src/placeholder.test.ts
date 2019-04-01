6b810e1156757e06f31b62d054f7dbf8e1b048d8
Shobhit <shobhit657@gmail.com>
2019-04-01 15:17:49 -0700

    Add jest config and setup (#4)
    * Add jest config and setup
    
    * add watch step
    
    * update jest config
    
    * changes based on PR comments
    
    * pivot test folder strategy
    
    * remove reference for projects since we dont need it anymore

---
+++ src/placeholder.test.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { sum } from './placeholder';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
