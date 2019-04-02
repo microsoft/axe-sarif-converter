b5ba986ea193106581df92e8cc6afc3757b77f1e
Dan Bjorge <dbjorge@gmail.com>
2019-04-02 13:55:09 -0700

    Enable tslint (#7)
    * Add lint script and integrate to prepack
    
    * Fix pre-existing lint errors
    
    * add tslint as a devDependency

--- src/placeholder.ts
+++ src/placeholder.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-export function sum(a: number, b: number): number {
+export function placeholder(a: number, b: number): number {
     return a + b;
 }
