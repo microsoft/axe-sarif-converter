e47b54b318ff64b0bad83956b9625266a6246941
Longhan Li <lonli@microsoft.com>
2019-04-02 09:39:21 -0700

    Rmoved extra empty lines.

--- src/placeholder.ts
+++ src/placeholder.ts
@@ -1,8 +1,3 @@
-/*!
- * //Copyright (c) Microsoft Corporation. All rights reserved.
- * //Licensed under the MIT License.
- */
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 export function sum(a: number, b: number): number {
