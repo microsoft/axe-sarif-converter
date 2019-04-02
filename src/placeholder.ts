28d9a830e96e642f6955fc7f5543814be0dc9860
Longhan Li <lonli@microsoft.com>
2019-04-02 09:35:53 -0700

    Remove copy header fix to avoid using grunt

--- src/placeholder.ts
+++ src/placeholder.ts
@@ -1,3 +1,8 @@
+/*!
+ * //Copyright (c) Microsoft Corporation. All rights reserved.
+ * //Licensed under the MIT License.
+ */
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 export function sum(a: number, b: number): number {
