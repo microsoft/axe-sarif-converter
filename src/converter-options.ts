f3e4286c5a08d6231ac7fe883a176dae29d2cb1f
Dan Bjorge <danielbj@microsoft.com>
2019-04-05 14:21:48 -0700

    Remove unnecessary code related to scanning

---
+++ src/converter-options.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+export interface ConverterOptions {
+    testCaseId?: string;
+    scanId?: string;
+    scanName?: string;
+}
