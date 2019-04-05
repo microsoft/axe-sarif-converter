1ab80dd6f1e968982347b4c11c3d9da8ba02a58f
Dan Bjorge <dbjorge@gmail.com>
2019-04-05 14:40:52 -0700

    Remove unnecessary code related to scanning (#17)
    * Remove unnecessary code related to scanning
    * Update snapshot

--- src/result-decorator.ts
+++ src/result-decorator.ts
@@ -2,25 +2,12 @@
 // Licensed under the MIT License.
 import * as Axe from 'axe-core';
 import { DictionaryStringTo } from './dictionary-types';
-import { DocumentUtils } from './document-utils';
-import { MessageDecorator } from './message-decorator';
 import { Processor } from './processor';
 import { AxeCoreRuleResult, AxeRule, ScannerResults } from './ruleresults';
 import { WCAG } from './wcag';
 
 export class ResultDecorator {
-    private wcagConfiguration!: DictionaryStringTo<WCAG[]>;
-    private _documentUtils: DocumentUtils;
-    private _messageDecorator: MessageDecorator;
-
-    constructor(
-        documentUtils: DocumentUtils,
-        messageDecorator: MessageDecorator,
-        private getHelpUrl: (rule: string) => string,
-    ) {
-        this._documentUtils = documentUtils;
-        this._messageDecorator = messageDecorator;
-    }
+    constructor(private wcagConfiguration: DictionaryStringTo<WCAG[]>) {}
 
     public decorateResults(results: Axe.AxeResults): ScannerResults {
         return {
@@ -37,19 +24,12 @@ export class ResultDecorator {
         };
     }
 
-    public setWCAGConfiguration(
-        configuration: DictionaryStringTo<WCAG[]>,
-    ): void {
-        this.wcagConfiguration = configuration;
-    }
-
     private decorateAxeRuleResults(
         ruleResults: AxeRule[],
         isInapplicable: boolean = false,
     ): AxeCoreRuleResult[] {
         return ruleResults.reduce<AxeCoreRuleResult[]>(
             (filteredArray, result: AxeRule) => {
-                this._messageDecorator.decorateResultWithMessages(result);
                 const processedResult = Processor.suppressChecksByMessages(
                     result,
                     !isInapplicable,
@@ -59,7 +39,6 @@ export class ResultDecorator {
                     filteredArray.push({
                         ...processedResult,
                         WCAG: this.getRelatedWCAGByRuleId(result.id),
-                        helpUrl: this.getHelpUrl(result.id),
                     });
                 }
 
