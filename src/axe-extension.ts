828e7c680552819879ecb3848882d912dd50abc3
Lisa Li <lisli@microsoft.com>
2019-04-05 14:40:43 -0700

    Moved axe raw result typings to axe-extension

--- src/axe-extension.ts
+++ src/axe-extension.ts
@@ -30,4 +30,46 @@ declare module 'axe-core/axe' {
             checks: CheckConfiguration[];
         };
     };
+
+    export interface AxeRawResult {
+        id: string;
+        result: string;
+        pageLevel: boolean;
+        impact?: Impact;
+        tags: string[];
+        help: string;
+        helpUrl: string;
+        description: string;
+        inapplicable: AxeRawNodeResult[];
+        violations?: AxeRawNodeResult[];
+        passes?: AxeRawNodeResult[];
+        incomplete?: AxeRawNodeResult[];
+    }
+
+    export interface AxeRawNodeResult {
+        any: AxeRawCheck[];
+        all: AxeRawCheck[];
+        none: AxeRawCheck[];
+        node: AxeRawNode;
+        impact?: Impact;
+        result?: 'passed' | 'failed' | 'inapplicable' | 'incomplete';
+    }
+
+    export interface AxeRawCheck {
+        id: string;
+        data?: string | object;
+        relatedNodes: AxeRawNode[];
+        impact: Impact;
+        message?: string;
+    }
+
+    export interface AxeRawNode {
+        selector: Selector;
+    }
+
+    export type Impact = 'minor' | 'moderate' | 'serious' | 'critical';
+
+    export type SelectorItem = string[] | string;
+
+    export type Selector = SelectorItem[];
 }
