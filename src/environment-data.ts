8f2bcfbf2c2a58bd920834203521c697255c692d
Lisa Li <lisli@microsoft.com>
2019-04-10 18:51:30 -0700

    Added copyright header to environment-data

--- src/environment-data.ts
+++ src/environment-data.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 export interface EnvironmentData {
     targetPageUrl: string;
     targetPageTitle: string;
