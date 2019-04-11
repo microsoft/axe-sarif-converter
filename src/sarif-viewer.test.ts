0ee29b3853b97c3b982880d86dd7f5d44adb6379
Longhan Li <lonli@microsoft.com>
2019-04-11 10:05:43 -0700

    Fixed pr comments.

--- src/sarif-viewer.test.ts
+++ src/sarif-viewer.test.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import { AxeResults } from 'axe-core';
 import * as fs from 'fs';
 import { convertAxeToSarif } from './index';
@@ -11,12 +10,17 @@ describe('Azure DevOps sarif viewer extension test', () => {
             './src/test-resources/axe322-v2.dequemars-testsite.1554329251110.json',
             'utf8',
         );
-        const axeResultStub: AxeResults = JSON.parse(axeJSON) as AxeResults;
-        fs.mkdir('./test-results', () => {
-            fs.writeFileSync(
-                './test-results/test_result.sarif',
-                JSON.stringify(convertAxeToSarif(axeResultStub)),
-            );
-        });
+        const axeResult: AxeResults = JSON.parse(axeJSON) as AxeResults;
+        if (!fs.existsSync('./test-results')) {
+            fs.mkdirSync('./test-results');
+        }
+
+        fs.writeFileSync(
+            './test-results/test_result.sarif',
+            JSON.stringify(convertAxeToSarif(axeResult)),
+            {
+                encoding: 'utf8',
+            },
+        );
     });
 });
