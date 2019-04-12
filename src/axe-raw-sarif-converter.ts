23c43ef00603366bd7e24b09bba25c5132465d78
Lisa Li <lisli@microsoft.com>
2019-04-12 15:39:48 -0700

    Changed import to use isEmpty from array-utils

--- src/axe-raw-sarif-converter.ts
+++ src/axe-raw-sarif-converter.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { isEmpty } from 'lodash';
+import { isEmpty } from './array-utils';
 import {
     AxeRawCheckResult,
     AxeRawNodeResult,
