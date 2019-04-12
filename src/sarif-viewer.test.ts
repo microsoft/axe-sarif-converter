b91a954e01e166bb56e1e5df72235d8a9cdf25b5
mslonli <lonli@microsoft.com>
2019-04-11 18:50:58 -0700

    Sarif viewer extension tab test (#24)
    * Added copyright check and fix
    
    * parital changes
    
    * Remove copy header fix to avoid using grunt
    
    * Rmoved extra empty lines.
    
    * Rmoved extra npm install
    
    * fixed pr comments
    
    * Added PR template.
    
    * Removed copyright headers from pr template.
    
    * Fixed pr comments.
    
    * sarif viewer extension test
    
    * sarif viewer experiment
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * test commit
    
    * Added test to generate sarif viewer data.
    
    * Update api name after merge with master.
    
    * Clean unnecessary codes.
    
    * Removed unnecessary code change.
    
    * Removed unnecessary changes.
    
    * Removed unnecessary changes.
    
    * Fixed pr comments.

---
+++ src/sarif-viewer.test.ts
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { AxeResults } from 'axe-core';
+import * as fs from 'fs';
+import { convertAxeToSarif } from './index';
+
+describe('Azure DevOps sarif viewer extension test', () => {
+    it('save sarif file to artifact to be shown by sarif viewer tab', () => {
+        const axeJSON: string = fs.readFileSync(
+            './src/test-resources/axe322-v2.dequemars-testsite.1554329251110.json',
+            'utf8',
+        );
+        const axeResult: AxeResults = JSON.parse(axeJSON) as AxeResults;
+        if (!fs.existsSync('./test-results')) {
+            fs.mkdirSync('./test-results');
+        }
+
+        fs.writeFileSync(
+            './test-results/test_result.sarif',
+            JSON.stringify(convertAxeToSarif(axeResult)),
+            {
+                encoding: 'utf8',
+            },
+        );
+    });
+});
