6aff5e4b60c774fc24a2e10bdc6f9891d16fd408
Lisa Li <lisli@microsoft.com>
2019-04-19 16:53:08 -0700

    Started adding axe reporter wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as Axe from 'axe-core';
+import { AxeRawResult } from './axe-raw-result';
+import { defaultAxeRawSarifConverter } from './axe-raw-sarif-converter';
+import { EnvironmentData } from './environment-data';
 import { ResultDecorator } from './result-decorator';
 import { defaultSarifConverter } from './sarif-converter';
 import { SarifLog } from './sarif/sarif-log';
@@ -14,4 +17,25 @@ export function convertAxeToSarif(axeResults: Axe.AxeResults): SarifLog {
 
     const sarifConverter = defaultSarifConverter();
     return sarifConverter.convert(decoratedAxeResults, {});
+}
+
+export function sarifReporter(
+    rawResults: AxeRawResult[],
+    {},
+    callback: Function,
+) {
+    let environmentData: EnvironmentData = {
+        timestamp: '',
+        targetPageUrl: '',
+        targetPageTitle: '',
+    };
+    callback(createSarifReport(rawResults, environmentData));
+}
+
+export function createSarifReport(
+    rawResults: AxeRawResult[],
+    environmentData: EnvironmentData,
+): SarifLog {
+    const axeRawSarifConverter = defaultAxeRawSarifConverter();
+    return axeRawSarifConverter.convert(rawResults, {}, environmentData);
 }
