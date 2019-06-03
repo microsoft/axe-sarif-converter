53b90ff286e9aff34c7f955995d3a0a4edc3d397
Lisa Li <lisli@microsoft.com>
2019-06-03 14:53:57 -0700

    Added new invocation provider and sarif converter for sarif 2.1.2

---
+++ src/invocation-provider-21.test.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { EnvironmentData } from './environment-data';
+import { getInvocations21 } from './invocation-provider-21';
+import * as Sarif from './sarif/sarif-2.1.2';
+
+describe('invocation-provider-2.1', () => {
+    describe('getInvocations21', () => {
+        it('populates invocations with data from environmentData parameter', () => {
+            const environmentDataStub: EnvironmentData = {
+                targetPageUrl: 'https://example.com',
+                targetPageTitle: 'Environment Data Stub',
+                timestamp: '2018-03-23T21:36:58.321Z',
+            };
+            const invocationStub: Sarif.Invocation[] = [
+                {
+                    startTimeUtc: '2018-03-23T21:36:58.321Z',
+                    endTimeUtc: '2018-03-23T21:36:58.321Z',
+                    executionSuccessful: true,
+                },
+            ];
+            const actualResults: Sarif.Invocation[] = getInvocations21(
+                environmentDataStub,
+            );
+            expect(actualResults).toEqual(invocationStub);
+        });
+    });
+});
---
+++ src/sarif-converter-21.test.ts
@@ -0,0 +1,104 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { IMock, It, Mock, Times } from 'typemoq';
+import { getAxeToolProperties } from './axe-tool-property-provider';
+import { ConverterOptions } from './converter-options';
+import { DecoratedAxeResults } from './decorated-axe-results';
+import { EnvironmentData } from './environment-data';
+import { getInvocations } from './invocation-provider';
+import { SarifConverter } from './sarif-converter';
+import * as Sarif from './sarif/sarif-2.0.0';
+
+describe('SarifConverter21', () => {
+    describe('convert', () => {
+        const stubToolProperties: Sarif.Run['tool'] = {
+            name: 'stub_tool_property',
+        };
+        const stubInvocations: Sarif.Invocation[] = [
+            { commandLine: 'stub_invocation' },
+        ];
+        const stubTimestamp: string = 'stub_timestamp';
+        const stubTargetPageUrl: string = 'stub_url';
+        const stubTargetPageTitle: string = 'stub_title';
+        const stubEnvironmentData: EnvironmentData = {
+            timestamp: stubTimestamp,
+            targetPageUrl: stubTargetPageUrl,
+            targetPageTitle: stubTargetPageTitle,
+        };
+        const axeToolPropertyProviderStub: () => Sarif.Run['tool'] = () => {
+            return {} as Sarif.Run['tool'];
+        };
+        const invocationProviderStub: () => Sarif.Invocation[] = () => {
+            return stubInvocations;
+        };
+
+        it('outputs a sarif log whose run uses the axeToolPropertyProvider to populate the tool property', () => {
+            const axeToolPropertyProviderMock: IMock<
+                () => Sarif.Run['tool']
+            > = Mock.ofInstance(getAxeToolProperties);
+            axeToolPropertyProviderMock
+                .setup(ap => ap())
+                .returns(() => stubToolProperties)
+                .verifiable(Times.once());
+
+            const irrelevantResults: DecoratedAxeResults = {} as DecoratedAxeResults;
+            const irrelevantOptions: ConverterOptions = {};
+
+            const testSubject = new SarifConverter(
+                axeToolPropertyProviderMock.object,
+                invocationProviderStub,
+            );
+
+            const actualResults = testSubject.convert(
+                irrelevantResults,
+                irrelevantOptions,
+            );
+
+            axeToolPropertyProviderMock.verifyAll();
+            expect(actualResults).toHaveProperty('runs');
+            expect(actualResults.runs[0]).toHaveProperty(
+                'tool',
+                stubToolProperties,
+            );
+        });
+        it('outputs a sarif log whose run uses the invocationsProvider to populate the invocations property', () => {
+            const stubResults: DecoratedAxeResults = {
+                timestamp: stubTimestamp,
+                targetPageUrl: stubTargetPageUrl,
+                targetPageTitle: stubTargetPageTitle,
+                passes: [],
+                violations: [],
+                inapplicable: [],
+                incomplete: [],
+            };
+            const irrelevantOptions: ConverterOptions = {};
+
+            const invocationProviderMock: IMock<
+                (environmentData: EnvironmentData) => Sarif.Invocation[]
+            > = Mock.ofInstance(getInvocations);
+            invocationProviderMock
+                .setup(ip =>
+                    ip(It.isObjectWith<EnvironmentData>(stubEnvironmentData)),
+                )
+                .returns(() => stubInvocations)
+                .verifiable(Times.once());
+
+            const testSubject = new SarifConverter(
+                axeToolPropertyProviderStub,
+                invocationProviderMock.object,
+            );
+
+            const actualResults = testSubject.convert(
+                stubResults,
+                irrelevantOptions,
+            );
+
+            invocationProviderMock.verifyAll();
+            expect(actualResults).toHaveProperty('runs');
+            expect(actualResults.runs[0]).toHaveProperty(
+                'invocations',
+                stubInvocations,
+            );
+        });
+    });
+});
