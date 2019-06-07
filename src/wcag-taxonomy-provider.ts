cfa8a27ea877171e48aac31ac1145cfc25baf756
Lisa Li <lisli@microsoft.com>
2019-06-07 16:08:21 -0700

    Created wcag-link-data-processor and wcag-taxonomy-provider

---
+++ src/wcag-taxonomy-provider.ts
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import * as Sarif from './sarif/sarif-2.1.2';
+import { axeTagsToWcagLinkData, WCAGLinkData } from './wcag-link-data';
+
+export class WCAGTaxonomyProvider {
+    public getWcagTaxonomy(sortedWcagTags: string[]): Sarif.ToolComponent {
+        return {
+            name: 'WCAG',
+            fullName: 'Web Content Accessibility Guidelines (WCAG) 2.1',
+            organization: 'W3C',
+            informationUri: 'https://www.w3.org/TR/WCAG21',
+            version: '2.1',
+            guid: '',
+            isComprehensive: true,
+            taxa: this.getAllTaxaFromWcagLinkData(sortedWcagTags),
+        };
+    }
+
+    private getAllTaxaFromWcagLinkData(
+        sortedWcagTags: string[],
+    ): Sarif.ToolComponent['taxa'] {
+        var taxa: Sarif.ToolComponent['taxa'] = [];
+        for (let i = 0; i < sortedWcagTags.length; i++) {
+            var tag = sortedWcagTags[i];
+            taxa.push(
+                this.getIndividualTaxaFromWcagLinkData(
+                    tag,
+                    axeTagsToWcagLinkData[tag],
+                ),
+            );
+        }
+        return taxa;
+    }
+
+    private getIndividualTaxaFromWcagLinkData(
+        tag: string,
+        wcagLinkData: WCAGLinkData,
+    ): Sarif.ReportingDescriptor {
+        return {
+            id: tag,
+            name: wcagLinkData.text,
+            shortDescription: {
+                text: wcagLinkData.title ? wcagLinkData.title : '',
+            },
+            helpUri: wcagLinkData.url,
+        };
+    }
+}
