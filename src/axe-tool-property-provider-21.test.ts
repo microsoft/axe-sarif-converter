1e80c898c7036086817fc5a8172f60cc97cd06ad
Lisa Li <lisli@microsoft.com>
2019-06-13 16:18:08 -0700

    Updated axe-tool-property-provider to work better with populating rules

--- src/axe-tool-property-provider-21.test.ts
+++ src/axe-tool-property-provider-21.test.ts
@@ -6,34 +6,31 @@ import { WcagGuid } from './wcag-taxonomy-provider';
 
 describe('axe-tool-property-provider 2.1', () => {
     describe('getAxeToolProperties21', () => {
-        it("returns the axe properties as a Sarif.Run['tool']", () => {
-            const expectedResults: Sarif.Run['tool'] = {
-                driver: {
-                    name: 'axe-core',
-                    fullName: 'axe for Web v3.2.2',
-                    shortDescription: {
-                        text:
-                            'An open source accessibility rules library for automated testing.',
-                    },
-                    version: '3.2.2',
-                    semanticVersion: '3.2.2',
-                    informationUri: 'https://www.deque.com/axe/axe-for-web/',
-                    downloadUri:
-                        'https://www.npmjs.com/package/axe-core/v/3.2.2',
-                    properties: {
-                        'microsoft/qualityDomain': 'Accessibility',
-                    },
-                    supportedTaxonomies: [
-                        {
-                            name: 'WCAG',
-                            index: 0,
-                            guid: WcagGuid,
-                        },
-                    ],
+        it('returns the axe properties as a Sarif.ToolComponent', () => {
+            const expectedResults: Sarif.ToolComponent = {
+                name: 'axe-core',
+                fullName: 'axe for Web v3.2.2',
+                shortDescription: {
+                    text:
+                        'An open source accessibility rules library for automated testing.',
                 },
+                version: '3.2.2',
+                semanticVersion: '3.2.2',
+                informationUri: 'https://www.deque.com/axe/axe-for-web/',
+                downloadUri: 'https://www.npmjs.com/package/axe-core/v/3.2.2',
+                properties: {
+                    'microsoft/qualityDomain': 'Accessibility',
+                },
+                supportedTaxonomies: [
+                    {
+                        name: 'WCAG',
+                        index: 0,
+                        guid: WcagGuid,
+                    },
+                ],
             };
 
-            const actualResults: Sarif.Run['tool'] = getAxeToolProperties21();
+            const actualResults: Sarif.ToolComponent = getAxeToolProperties21();
             expect(actualResults).toEqual(expectedResults);
         });
     });
