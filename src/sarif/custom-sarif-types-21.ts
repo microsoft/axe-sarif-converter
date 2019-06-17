8049b1878b7ae8d6d3406334a8a68a62ceb57868
Lisa Li <lisli@microsoft.com>
2019-06-17 13:00:08 -0700

    remove unused message type from custom sarif types

--- src/sarif/custom-sarif-types-21.ts
+++ src/sarif/custom-sarif-types-21.ts
@@ -21,33 +21,3 @@ export namespace Result {
         error = 'error',
     }
 }
-
-/**
- * Encapsulates a message intended to be read by the end user.
- */
-export interface Message {
-    /**
-     * A plain text message string.
-     */
-    text?: string;
-
-    /**
-     * The resource id for a plain text message string.
-     */
-    messageId?: string;
-
-    /**
-     * A rich text message string.
-     */
-    richText?: string;
-
-    /**
-     * The resource id for a rich text message string.
-     */
-    richMessageId?: string;
-
-    /**
-     * An array of strings to substitute into the message string.
-     */
-    arguments?: string[];
-}
