144e6e9bb22177854d7c7a2e048ef34d950c4c09
Dan Bjorge <danielbj@microsoft.com>
2019-06-21 12:17:47 -0700

    Leave the SarifLog type alias in place for parity

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as Axe from 'axe-core';
-import { Log } from 'sarif';
+import * as Sarif from 'sarif';
 import { AxeRawResult } from './axe-raw-result';
 import { defaultAxeRawSarifConverter21 } from './axe-raw-sarif-converter-21';
 import { ConverterOptions } from './converter-options';
@@ -9,9 +9,9 @@ import { EnvironmentData } from './environment-data';
 import { getEnvironmentDataFromEnvironment } from './environment-data-provider';
 import { defaultSarifConverter21 } from './sarif-converter-21';
 
-export { Log } from 'sarif';
+export type SarifLog = Sarif.Log;
 
-export function convertAxeToSarif(axeResults: Axe.AxeResults): Log {
+export function convertAxeToSarif(axeResults: Axe.AxeResults): SarifLog {
     const sarifConverter = defaultSarifConverter21();
     return sarifConverter.convert(axeResults, {});
 }
@@ -19,7 +19,7 @@ export function convertAxeToSarif(axeResults: Axe.AxeResults): Log {
 export function sarifReporter(
     rawResults: AxeRawResult[],
     runOptions: Axe.RunOptions,
-    callback: (sarifResults: Log) => void,
+    callback: (sarifResults: SarifLog) => void,
 ) {
     const converterOptions: ConverterOptions = {};
     const environmentData: EnvironmentData = getEnvironmentDataFromEnvironment();
