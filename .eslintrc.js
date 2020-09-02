59a15001fe959fd207f267da351130241524ffd1
Rob Gallo <43080011+RobGallo@users.noreply.github.com>
2020-09-02 17:16:27 -0400

    feat(compliance-web): Migrate project from tslint to eslint (#387)
    * feat(compliance-web): Migrate project from tslint to eslint
    
    - Add .eslintrc.js
    - remove tsling.config.json
    - Disable failing rules (to keep PR small and to be fixed later)
    - Add recommended plugins for vscode
    - Remove some excluded files from tsconfig.json because eslint throws an error if the files are not included (we can change this to exclude them from scanning altogether if we want, but it seems good to scan them if possible)
    
    Note: Security rules will be fixed in an upcoming PR.
    
    * Chore: Re-add test files to the exclude list in tsconfig.json
    
    The previous change caused yarn build to fail because it was picking up the contents of src/test-resources/generator/, which it's not supposed to do; that directory is a tool for generating new test resources, has its own separate package.json, and is intended to be built separately
    
    The addition of tsconfig.eslint.json allows test files to be included in the eslint scan, but remain excluded for the build.

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+module.exports = {
+    env: {
+        es2017: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:security/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module', // Allows for the use of imports
+        project: './tsconfig.eslint.json',
+        ecmaFeatures: {},
+        ecmaVersion: 8,
+    },
+    plugins: ['@typescript-eslint', 'security'],
+    settings: {},
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        'security/detect-object-injection': 'off',
+        'security/detect-non-literal-fs-filename': 'off',
+        'no-var': 'off',
+        'prefer-const': 'off',
+    },
+    overrides: [
+        {
+            files: ['src/tests/**/*'],
+            rules: {
+                // Disable those errors and warnings which are not a threat to test code because the code is not run in production environments
+            },
+        },
+    ],
+};
