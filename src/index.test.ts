1efff1de3652129b5b9576cc8e544f4b46427d01
Jeevani Chinthala <v-jeevanic@microsoft.com>
2024-09-18 15:35:52 +0530

    feat(axe-core-4.10.0): update to use axe-core 4.10.0

--- src/index.test.ts
+++ src/index.test.ts
@@ -123,6 +123,10 @@ describe('public convertAxeToSarif API', () => {
         ${'basic-axe-v4.9.1.reporter-v2.json'}        | ${'basic-axe-v4.9.1.sarif'}
         ${'w3citylights-axe-v4.9.1.reporter-v1.json'} | ${'w3citylights-axe-v4.9.1.sarif'}
         ${'w3citylights-axe-v4.9.1.reporter-v2.json'} | ${'w3citylights-axe-v4.9.1.sarif'}
+        ${'basic-axe-v4.10.0.reporter-v1.json'}        | ${'basic-axe-v4.10.0.sarif'}
+        ${'basic-axe-v4.10.0.reporter-v2.json'}        | ${'basic-axe-v4.10.0.sarif'}
+        ${'w3citylights-axe-v4.10.0.reporter-v1.json'} | ${'w3citylights-axe-v4.10.0.sarif'}
+        ${'w3citylights-axe-v4.10.0.reporter-v2.json'} | ${'w3citylights-axe-v4.10.0.sarif'}
     `(
         'converts pinned v1/v2 input $inputFile to pinned output $outputFile',
         ({ inputFile, outputFile }) => {
@@ -169,7 +173,7 @@ describe('public sarifReporter API', () => {
     // it isn't very meaningful to test cases that involve old axe versions here.
     it.each`
         inputFile                               | outputFile
-        ${'basic-axe-v4.9.1.reporter-raw.json'} | ${'basic-axe-v4.9.1.sarif'}
+        ${'basic-axe-v4.10.0.reporter-raw.json'} | ${'basic-axe-v4.10.0.sarif'}
     `(
         'converts pinned raw input $inputFile to pinned output $outputFile',
         async ({ inputFile, outputFile }) => {
