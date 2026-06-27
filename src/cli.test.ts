9bffcb13a6e995e184864fccce23a5b2a562a2c7
Chirag Sharma <150002431+v-sharmachir@users.noreply.github.com>
2026-06-27 14:24:13 +0530

    feat(axe-core): updated axe-core to 4.12.1 (#1746)
    #### Details
    
    This PR upgrades axe-core to 4.11.3 according to the instructions in
    CONTRIBUTING.md.
    
    ##### Motivation
    
    Axe-core update to latest version
    
    ##### Context
    
    <!-- Are there any parts that you've intentionally left out-of-scope for
    a later PR to handle? -->
    
    <!-- Were there any alternative approaches you considered? What
    tradeoffs did you consider? -->
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [x] PR title respects [Conventional
    Commits](https://www.conventionalcommits.org) (starts with `fix:`,
    `feat:`, etc, and is suitable for user-facing release notes)
    - [x] PR contains no breaking changes, **OR** description of both PR
    **and final merge commit** starts with `BREAKING CHANGE:`
    - [x] Verify PR title and final merge commit contain space after `:` for
    example `feat(feature): feature title` otherwise it will not be
    considered by semantic-release for release.
    - [ ] (if applicable) Addresses issue: #0000
    - [x] Added relevant unit tests for your changes
    - [x] Ran `yarn precheckin`
    - [x] Verified code coverage for the changes made

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -93,6 +93,8 @@ describe('axe-sarif-converter CLI', () => {
         ${'w3citylights-axe-v4.10.2.axe-cli-v4.10.2.json'}
         ${'basic-axe-v4.11.3.axe-cli-v4.11.2.json'}
         ${'w3citylights-axe-v4.11.3.axe-cli-v4.11.2.json'}
+        ${'basic-axe-v4.12.1.axe-cli-v4.12.1.json'}
+        ${'w3citylights-axe-v4.12.1.axe-cli-v4.12.1.json'}
     `(
         'supports conversion from axe-cli output $inputFile',
         async ({ inputFile }) => {
@@ -214,11 +216,11 @@ describe('axe-sarif-converter CLI', () => {
     const testResultsDir = path.join(__dirname, '..', 'test-results');
     const basicAxeV2File = path.join(
         testResourcesDir,
-        'basic-axe-v4.11.3.reporter-v2.json',
+        'basic-axe-v4.12.1.reporter-v2.json',
     );
     const basicSarifFile = path.join(
         testResourcesDir,
-        'basic-axe-v4.11.3-v2.sarif',
+        'basic-axe-v4.12.1-v2.sarif',
     );
 
     const mkdir = promisify(fs.mkdir);
